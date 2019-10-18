5bf3352a87d453a201ef59813de5b19b3b3ede5c
Leo <coder.leo.le@gmail.com>
2019-10-19 00:15:22 +0700

    Configure eslint + prettier + typescript complier

---
+++ client/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  semi: false,
+  jsxSingleQuote: false,
+  singleQuote: true,
+  trailingComma:  'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+}
