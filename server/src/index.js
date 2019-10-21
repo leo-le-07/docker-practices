b8fc95c1cecf41ac87216dbf729da6a6a5e75af3
Leo <coder.leo.le@gmail.com>
2019-10-21 21:43:07 +0700

    Allow CORS server

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
 import { ApolloServer, gql } from 'apollo-server-express'
+import cors from 'cors'
 
 import { typeDefs, resolvers } from './controllers'
 import db from './models'
@@ -12,10 +13,27 @@ const apolloServer = new ApolloServer({
   context: { db },
 })
 
+const allowedOrigins = [
+  `http://localhost:${process.env.WEB_URI}`,
+]
+
 const app = express()
 apolloServer.applyMiddleware({ app })
 
 app.use(morgan('combined'))
+app.use(cors({
+  origin(origin, callback) {
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(!allowedOrigins.includes(origin)){
+      const msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
